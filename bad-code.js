875a60974dca8a5e8cf6c197cf55464a8c82959d
ru58841 <21xva21@gmail.com>
2026-03-29 10:35:37 +0000

    plswork

--- bad-code.js
+++ bad-code.js
@@ -4,10 +4,6 @@ function add(a, b) {
     return a + c;  
 }
 
-function getUser(userId) {
-    const query = "SELECT * FROM users WHERE id = " + userId;  
-    db.execute(query);
-}
 
 var x = undefined;
 console.log(x.name);  
