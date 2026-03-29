24db08a35bb463e403d24581cf33e8272dfca061
ru58841 <21xva21@gmail.com>
2026-03-29 10:54:28 +0000

    last

--- bad-code.js
+++ bad-code.js
@@ -1,13 +1,32 @@
-const DB_PASSWORD = "supersecret123";  // hardcoded secret
-
-function add(a, b) {
-    return a + c;  // wrong variable
+function calculateTotal(a, b, c) {
+    var result = a + b + c
+    return result
 }
 
-function getUser(userId) {
-    const query = "SELECT * FROM users WHERE id = " + userId;  // sql injection
-    db.execute(query);
+function fetchData() {
+    fetch("https://api.example.com/data")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
 }
 
-var x = undefined;
-console.log(x.name);  // will crash, can't read property of undefined
+function login(user, pass) {
+    if (user == "admin" && pass == "1234") {
+        return true
+    } else {
+        return false
+    }
+}
+
+function temp() {
+    let x = 10
+}
+
+function x(y) {
+    return y * 42
+}
+
+for (let i = 0; i < 1000000000; i++) {
+    console.log(i)
+}
